b6a37c3ea4d211fe7f52213b89b5022f718f782c
Toyoyo <toyoyo587@gmail.com>
2026-02-03 23:22:43 +0900

    Initial commit

---
+++ abc/abc419/abc419c/abc419c.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+       long long n;
+       cin >> n;
+       vector<long long> r(n),c(n);
+       for(int i=0;i < n;i++){
+            cin >> r[i] >> c[i];
+       }
+
+}
+
---
+++ abc/abc420/abc420c/abc420c.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+   long long n,q;
+   cin >> n >> q;
+   vector<long long> a(n),b(n);
+   for(int i=0;i < n;i++){
+        cin >> a.at(i);
+   }
+   for(int i=0;i < n;i++){
+        cin >> b.at(i);
+   }
+
+   long long sum = 0;
+   for(int i=0;i < n;i++){
+        sum += min(a.at(i),b.at(i));
+   }
+
+   for(int i=0;i < q;i++){
+        char c;
+        long long x,v;
+        cin >> c >> x >> v;
+        
+        if(c == 'A'){
+            sum -= min(a[x-1],b[x-1]);
+            a[x-1] = v;
+            sum += min(a[x-1],b[x-1]);
+        }else if(c == 'B'){ 
+            sum -= min(a[x-1],b[x-1]);
+            b[x-1] = v;
+            sum += min(a[x-1],b[x-1]);
+        }
+
+        cout << sum << endl;
+   }
+}
+
