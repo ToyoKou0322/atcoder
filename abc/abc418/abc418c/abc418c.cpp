86e316483d6cc2f316585805a6cdbd5f86c22d85
Toyoyo <toyoyo587@gmail.com>
2026-02-05 13:06:23 +0900

    不要ファイルの削除、問題の入力部分だけ記述

--- abc/abc418/abc418c/abc418c.cpp
+++ abc/abc418/abc418c/abc418c.cpp
@@ -5,6 +5,16 @@
 using namespace std;
 
 int main(){
-    
+    long long n,q;
+    cin >> n >> q;
+    vector<long long> a(n),b(q);
+    for(int i=0;i < n;i++){
+        cin >> a[i];
+    }
+    for(int i=0;i < q;i++){
+        cin >> b[i];
+    }
+
+
 }
 
