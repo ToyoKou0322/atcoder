5e72f3c08daab0b99c8fc865033147827cec609d
Toyoyo <257558686+ToyoKou0322@users.noreply.github.com>
2026-02-11 19:05:03 +0900

    C問題１問解いた

--- abc/abc418/abc418c/abc418c.cpp
+++ abc/abc418/abc418c/abc418c.cpp
@@ -8,39 +8,36 @@ int main(){
     long long n,q;
     cin >> n >> q;
     vector<long long> a(n),b(q);
+    long long max_a = 0;
     for(int i=0;i < n;i++){
         cin >> a[i];
+        max_a = max(max_a,a[i]);
     }
     for(int i=0;i < q;i++){
         cin >> b[i];
     }
 
-    long long sum = 0;
-    long long a_max = 0;
-    for(int i=0;i < n;i++){
-        a_max = max(a[i],a_max);
-        sum += a[i];
-    }
-
     sort(a.begin(),a.end());
 
-    vector<long long> pre_a(n+1);
+    vector<long long> prefixSum(n+1);
     for(int i=0;i < n;i++){
-        pre_a[i+1] = pre_a[i] + a[i];
+        prefixSum[i+1] = prefixSum[i] + a[i];
     }
-    
+
     for(int i=0;i < q;i++){
-        if(b[i] > a_max){
+        if(b[i] > max_a){
             cout << -1 << endl;
             continue;
         }
-
+        long long ans = 0;
         auto it = lower_bound(a.begin(),a.end(),b[i]);
-        long long idx = distance(a.begin(),it);
-        long long ans = pre_a[idx] + b[i];
+        long long d = distance(a.begin(),it);
+        ans += prefixSum[d];
+
+        long long add = (b[i]-1)*(n-d)+1;
+        ans += add;
         cout << ans << endl;
     }
-
     return 0;
 }
 
