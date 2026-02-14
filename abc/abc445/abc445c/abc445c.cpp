1e508e8de728048c4b7bc3c8a5d2485219e5c09c
Toyoyo <257558686+ToyoKou0322@users.noreply.github.com>
2026-02-14 23:10:15 +0900

    abc445のC問題解いた

--- abc/abc445/abc445c/abc445c.cpp
+++ abc/abc445/abc445c/abc445c.cpp
@@ -2,8 +2,29 @@
 #include <vector>
 #include <algorithm>
 
+using namespace std;
 
 int main(){
-    
+   int n;
+   cin >> n;
+   vector<int> a(n);
+   for(int i=0;i < n;i++){
+        cin >> a[i];
+   }
+
+   vector<int> ans(n,0);
+   for(int i=n-1;i >= 0;i--){
+        if(a[i] == i+1){
+            ans[i] = i+1;
+        }else{
+            ans[i] = ans[a[i]-1];
+        }
+   }
+
+   for(int i=0;i < n;i++){
+        cout << ans[i] << " ";
+   }
+   return 0;
 }
+
 
