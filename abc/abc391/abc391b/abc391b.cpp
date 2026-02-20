92b116d21dff0886dfb5e3455c02298cfdd30997
Toyoyo <257558686+ToyoKou0322@users.noreply.github.com>
2026-02-20 21:58:19 +0900

    B問題を１問解いた

--- abc/abc391/abc391b/abc391b.cpp
+++ abc/abc391/abc391b/abc391b.cpp
@@ -5,6 +5,37 @@
 using namespace std;
 
 int main(){
-        
+    int n,m;
+    cin >> n >> m;
+    vector<vector<char>> s(n,vector<char>(n));
+    vector<vector<char>> t(m,vector<char>(m));
+
+    for(int i=0;i < n;i++){
+        for(int j=0;j < n;j++){
+            cin >> s[i][j];
+        }
+    }
+    for(int i=0;i < m;i++){
+        for(int j=0;j < m;j++){
+            cin >> t[i][j];
+        }
+    }
+
+    for(int i=0;i < n-m+1;i++){
+        for(int j=0;j < n-m+1;j++){
+            bool ok = true;
+            for(int k=0;k < m;k++){
+                for(int l=0;l < m;l++){
+                    if(s[i+k][j+l] != t[k][l]){
+                        ok = false;
+                    }
+                }
+            }
+            if(ok){
+                cout << i+1 << " " << j+1 << endl;
+                return 0;
+            }
+        }
+    }
 }
 
